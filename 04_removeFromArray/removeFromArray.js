bd95219349e8262bd5ea66db9f166728c5c30eb0
Clifton <clifton314@gmail.com>
2023-02-09 07:45:41 -0500

    rmv from array using i and k loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(input, ...moreArg) {
 
+    let output = input;
+    let arrLen = output.length;
+    let argLen = moreArg.length;
+
+    for (i = 0; i < argLen; i++) {
+
+        for (k = 0; k < arrLen; k++) {
+            if (output[k] === moreArg[i]) {
+                output.splice(k, 1);
+            }
+        }
+
+    }
+    return output;
 };
 
 // Do not edit below this line
