9b863eed12c05aa14d00ef6d64e8ac4ee9042b4f
Clifton <clifton314@gmail.com>
2023-02-09 07:21:04 -0500

    fxn written to reverse str w/ loop and .length

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let strlen = string.length - 1;
+    if (strlen < 1) {
+        return string;
+    }
+    
+    let reverse;
+    
+    for (let i = strlen; i >= 0; i--) {
+        if (i == strlen) {
+            reverse = string.slice(i);
+        }
+        else {
+            reverse += string.slice(i, (i+1))
+        }
+    }
 
+    return reverse;
 };
 
 // Do not edit below this line
