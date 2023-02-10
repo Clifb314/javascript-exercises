c3af74dadeb4064f2f2e3999d205f7febbf5f8f4
Clifton <clifton314@gmail.com>
2023-02-10 04:45:06 -0500

    temp exercise complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,33 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  let conv = (5 / 9);
+  let answer = (temp - 32) * conv;
+  
+  if (Math.floor(answer) === answer) {
+    return answer;
+  }
+  
+  else {
+  return Number(answer.toFixed(1));
+  }
+
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  let conv = (9 / 5);
+  let answer = (temp * conv) + 32;
+
+  if (Math.floor(answer) === answer) {
+    return answer;
+  }
+  
+  else {
+  return Number(answer.toFixed(1));
+  }
+
 };
 
 // Do not edit below this line
