b825d226d3c80955cc51d381f9c8df43572f3e37
Clifton <clifton314@gmail.com>
2023-02-14 06:23:04 -0500

    merge changes from codespace

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(newArr) {
+    for (person of newArr) {
+        if (person.yearOfDeath === undefined) {
+            let today = currentDate.getFullYear()
+            this.yearOfDeath = today
+        }
+    }
 
+    let livesArr = newArr.map((person) => {
+        livesArr.name = person.name
+        livesArr.lifeSpan = person.yearOfDeath - person.yearOfBirth
+    })
+
+    let sorted = livesArr.sort((a, b) => a.lifeSpan - b.lifeSpan)
+
+    return sorted[0]
 };
 
 // Do not edit below this line
