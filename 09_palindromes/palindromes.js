3d4373d81ca61fd3c4453fec862d7245494a89e6
Clifton <clifton314@gmail.com>
2023-02-14 06:06:41 -0500

    correct syntex errors and add fnx to remove punct

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,24 +1,28 @@
 const palindromes = function (word) {
-    let word = word.toLowerCase;
+    word = word.toLowerCase();
+    //remove special characters
+    //carat means a range, square brackets indicate a char class
+    word = word.replace(/[^a-z]/g, '')
     let wordArr = word.split(' ');
 
+
+    let oneWord = '';
     //take spaces out
     if (wordArr.length > 1){
-        let oneWord = '';
-        wordArr.forEach(item => {
-            oneWord += item;
-        })
-        else {let oneWord = word}
-    }
+        wordArr.forEach(item => oneWord += item)
+        
+    } else {oneWord = word}
 
     let length = oneWord.length;
     let reverse = '';
-    for (let i = (length - 1); i <= 0; i--) {
+    for (let i = (length - 1); i >= 0; i--) {
         reverse += oneWord.charAt(i);
     }
 
     return reverse === oneWord
 };
+
+palindromes('ZZZZ car, a man, a maracaz')
 
 // Do not edit below this line
 module.exports = palindromes;
