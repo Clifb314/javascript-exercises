4aa60319c65aa6ec64da4e61f8079d2e5cafce30
Clifton <clifton314@gmail.com>
2023-02-14 06:41:57 -0500

    fixed syntax errors, fixed date object

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,20 +1,48 @@
 const findTheOldest = function(newArr) {
     for (person of newArr) {
         if (person.yearOfDeath === undefined) {
-            let today = currentDate.getFullYear()
-            this.yearOfDeath = today
+            let today = new Date()
+            today = today.getFullYear()
+            person.yearOfDeath = today
         }
     }
 
     let livesArr = newArr.map((person) => {
-        livesArr.name = person.name
-        livesArr.lifeSpan = person.yearOfDeath - person.yearOfBirth
+        const container = {}
+        container.name = person.name
+        container.lifeSpan = person.yearOfDeath - person.yearOfBirth
+        return container
     })
 
-    let sorted = livesArr.sort((a, b) => a.lifeSpan - b.lifeSpan)
+    let sorted = livesArr.sort((a, b) => b.lifeSpan - a.lifeSpan)
 
     return sorted[0]
 };
+
+
+
+//for testing
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+  findTheOldest(people)
+//end testing
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
