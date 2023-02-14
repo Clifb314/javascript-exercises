45ca31761ef12bc9aa2c6ff5c4f792d4f03fb1d9
Clifton <clifton314@gmail.com>
2023-02-14 05:41:45 -0500

    merge changes from codespace

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let word = word.toLowerCase;
+    let wordArr = word.split(' ');
 
+    //take spaces out
+    if (wordArr.length > 1){
+        let oneWord = '';
+        wordArr.forEach(item => {
+            oneWord += item;
+        })
+        else {let oneWord = word}
+    }
+
+    let length = oneWord.length;
+    let reverse = '';
+    for (let i = (length - 1); i <= 0; i--) {
+        reverse += oneWord.charAt(i);
+    }
+
+    return reverse === oneWord
 };
 
 // Do not edit below this line
