6ddeb3f8c70517c15fd4485fc096e33f391ed82a
Clifton <clifton314@gmail.com>
2023-02-14 06:08:36 -0500

    merge changes from codespace

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if (position < 0) {return 'OOPS'}
+    if (position < 3) {return 1}
 
+    let seq = [1, 1]
+    //let a;
+    //let b;
+    //for (let i = 1; i < (position - 1); i++) {
+    //    a = seq[i-1];
+    //   b = seq[i];
+    //   seq.push(a + b);
+    //}
+
+    //return seq[(position - 1)]
+    // I think we're meant to do this with reduce. Maybe now because it steps through
+    //every value?
+    for (let i = 1; i < (position - 1); i++) {
+        seq.push(seq.reduce(getSum, 0))
+    }
+
+    function getSum(a, b) {
+        return a + b
+    }
 };
 
 // Do not edit below this line
